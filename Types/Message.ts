1c4c8ae8df38bcb4a22095b95bd94a7cd688ff5c
Aranyak Ghosh <aranyak@cxunicorn.com>
2022-03-10 19:05:01 +0400

    Temporary message types

---
+++ Types/Message.ts
@@ -0,0 +1,31 @@
+// Template Interface for RPC Message
+// Language: typescript
+// Description: Message sent to invoke RPC
+interface RPCMessage<T> {
+    Audience: string[];
+    AdditionalData: T;
+}
+
+interface AuthRequestMessage {
+    
+}
+
+// Template Interface for Socket Message
+// Language: typescript
+// Description: Message sent to 
+interface SocketMessage {
+    SocketIDs: string[];
+    Type: string;
+    Data?: any;
+    TransactionID: string;
+}
+
+// Template Interface for RPC Message
+// Language: typescript
+// Description: Message sent to invoke RPC
+interface BrokerMessage<T> {
+    RecipientIDs: string[];
+    Type: string;
+    Data?: T;
+    TransactionID: string;
+}
