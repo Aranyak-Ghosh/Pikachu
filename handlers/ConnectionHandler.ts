77a4688e8115d5ad56a6852452b7a89b0d077ac1
Aranyak Ghosh <aranyak@cxunicorn.com>
2022-03-10 19:05:45 +0400

    Placeholder connection handler

---
+++ handlers/ConnectionHandler.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+import { AGServerSocket } from "socketcluster-server";
+import { SocketServerOptions } from "../types/SocketServerOptions";
+
+async function onClientConnectedHandler(
+    socket: AGServerSocket,
+    serverOptions: SocketServerOptions
+) {
+    for await (let request of socket.procedure("login")) {
+
+    }
+}
