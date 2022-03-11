1dca79c5a3dd92d8f9bb0298a3d5a1b2fa337dd4
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-03-11 10:25:40 +0400

    cries

--- Types/SocketConnection.ts
+++
@@ -1,9 +0,0 @@
-import { AGServerSocket } from "socketcluster-server";
-
-interface UserSocket {
-    UserID: string;
-    SessionID: string;
-    Socket: AGServerSocket
-}
-
-export type { UserSocket };
