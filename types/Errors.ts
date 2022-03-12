e323b8dd4749f4b7f95d95f1956dce6601d1e93a
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-03-13 02:33:26 +0400

    Refactor

--- types/Errors.ts
+++ types/Errors.ts
@@ -1,16 +1,15 @@
 class ClientNotInitializedError extends Error {
-    constructor(ClientType: string){
-        super(ClientType + " Client not initialized")
-        this.name = ClientNotInitializedError.name
+    constructor(ClientType: string) {
+        super(ClientType + " Client not initialized");
+        this.name = ClientNotInitializedError.name;
     }
 }
-
 
 class RedisPublishError extends Error {
-    constructor(){
-        super("Failed to publish error")
-        this.name = RedisPublishError.name
+    constructor() {
+        super("Failed to publish error");
+        this.name = RedisPublishError.name;
     }
 }
 
-export {ClientNotInitializedError, RedisPublishError}
+export { ClientNotInitializedError, RedisPublishError };
