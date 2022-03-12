ff1b76174143cb6beb55c00f75ae6f23a1a086c7
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-03-13 03:36:19 +0400

    Added redis client tests

--- utils/db/tests/RedisClient.test.ts
+++ utils/db/tests/RedisClient.test.ts
@@ -1,17 +1,44 @@
-import test, { ExecutionContext } from "ava";
-
 import { RedisClient } from "../RedisClient";
 
-// test.beforeEach((t) => {
-//   t.context = {
-//     client: RedisClient.Initialize(),
-//   };
-// });
+afterAll((done) => {
+    done();
+});
 
-const testSetValue = async (t: ExecutionContext, key: string, value: any) => {
-    var client = RedisClient.Initialize();
-    await client.SetKeyValueBuffered(key, value);
-    t.pass("View Redis Client");
-};
+describe("RedisClientTests", () => {
+    it("should write value as buffer to redis", async () => {
+        var client = RedisClient.Initialize();
+        const key = "foo";
+        const value = {
+            foo: "bar",
+        };
+        await client.SetKeyValueBuffered(key, value);
+        client.Disconnect();
+        return;
+    });
 
-test("Write data to Redis", testSetValue, "foo", { foo: "bar" });
+    it("should get value from redis", async () => {
+        var client = RedisClient.Initialize();
+        const key = "foo";
+        const value = {
+            foo: "bar",
+        };
+        await expect(client.GetValueForKey(key)).resolves.toBe(
+            JSON.stringify(value)
+        );
+
+        client.Disconnect();
+        return;
+    });
+
+    it("should publish to channel as buffer", async () => {
+        var client = RedisClient.Initialize();
+        var channel = "foo";
+        var data = {
+            foo: "bar",
+        };
+
+        await client.PublishMessage(channel, data);
+        client.Disconnect();
+        return;
+    });
+});
