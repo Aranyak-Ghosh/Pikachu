42f85e3b2d9576896142d4a822b55bf5b9e7b706
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-03-13 01:57:24 +0400

    refactor

--- utils/logger/ILogger.ts
+++ utils/logger/ILogger.ts
@@ -1,19 +1,22 @@
-import { LogLevel } from "../../types/Log"
+import { LogLevel } from "../../types/Log";
 
 abstract class ILogger {
-    public abstract Log(severity: LogLevel, message: string, ...args: any[]): void
-    public abstract Trace(message: string, ...args: any[]): void
-    public abstract Debug(message: string, ...args: any[]): void
-    public abstract Info(message: string, ...args: any[]): void
-    public abstract Warn(message: string, ...args: any[]): void
-    public abstract Error(message: string, ...args: any[]): void
+  public abstract Log(
+    severity: LogLevel,
+    message: string,
+    ...args: any[]
+  ): void;
+  public abstract Trace(message: string, ...args: any[]): void;
+  public abstract Debug(message: string, ...args: any[]): void;
+  public abstract Info(message: string, ...args: any[]): void;
+  public abstract Warn(message: string, ...args: any[]): void;
+  public abstract Error(message: string, ...args: any[]): void;
 
-    public abstract Error(ex: Error): void
+  public abstract Error(ex: Error): void;
 
-    public static GetInstance(): ILogger {
-        throw new Error("ILogger is an abstract class and can not be initialized")
-        return;
-    }
+  public static GetInstance(): ILogger {
+    throw new Error("ILogger is an abstract class and can not be initialized");
+  }
 }
 
-export { ILogger }
+export { ILogger };
