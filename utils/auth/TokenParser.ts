9adf7ab846ea49fd231589cb0c73f42e8ed63efe
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-03-14 22:31:50 +0400

    Added token parser with stub signature validation

--- utils/auth/TokenParser.ts
+++ utils/auth/TokenParser.ts
@@ -3,24 +3,49 @@ import { TokenValidationError } from "../../types/Errors";
 
 class TokenParser {
     private static ParseToken(token: string): TokenClaims {
-        let components = token.split(".");
+        let parts = token.split(".");
+        if (parts.length !== 3) throw new TokenValidationError();
         let claims: TokenClaims;
         claims = {
-            ...JSON.parse(Buffer.from(components[1], "base64").toString()),
-            ...JSON.parse(Buffer.from(components[0], "base64").toString()),
+            ...JSON.parse(Buffer.from(parts[0], "base64").toString()),
+            ...JSON.parse(Buffer.from(parts[1], "base64").toString()),
         };
         return claims;
     }
 
+    // private static ParseKey(publicKey: string) {
+    //     // let key = Buffer.from(publicKey,'base64')
+
+    //     // let length  = key.readUInt32BE()
+    //     // const format = key.subarray(4, 4 + length)				// ...and get format identifier...
+    //     // 	.toString('ascii');
+
+    //     let cert = new X509Certificate(publicKey);
+    //     console.log("Parsed certificate", cert);
+    // }
+
+    private static ValidateSignature(
+        token: string,
+        claims: TokenClaims
+    ): boolean {
+        //TODO: Implement signature verification
+        console.log(token, claims);
+        return true;
+    }
+
     public static ValidateToken(token: string): TokenClaims {
         let claims = TokenParser.ParseToken(token);
-
+        let validSignature = TokenParser.ValidateSignature(token, claims);
         let currentTime = Math.floor(Date.now() / 1000);
 
         if (
+            validSignature &&
             claims.nbf != null &&
             claims.nbf != undefined &&
-            claims.nbf < currentTime
+            claims.nbf <= currentTime &&
+            claims.exp != null &&
+            claims.exp != undefined &&
+            claims.exp > currentTime
         )
             return claims;
         else throw new TokenValidationError();
--- utils/auth/tests/TokenParser.test.ts
+++ utils/auth/tests/TokenParser.test.ts
@@ -3,7 +3,7 @@ import TokenParser from "../tokenParser";
 describe("TokenParserTests", () => {
     it("should parse token claims", () => {
         let data = {
-            exp: 1647278087,
+            exp: 1647285769,
             family_name: "A",
             alg: "RS256",
             kid: "5966c27282be2abd5a8f6e751ec582e3",
@@ -11,15 +11,15 @@ describe("TokenParserTests", () => {
             full_name: "Phani A",
             given_name: "Phani",
             hearing_id: [],
-            iat: 1647274487,
+            iat: 1647282169,
             iss: "authService",
-            nbf: 1647274487,
+            nbf: 1647282169,
             session_id: [],
             user_email: "phani29588@gmail.com",
             user_id: "74dedaaa-b965-421c-a31c-97793ba7f398",
         };
         let token =
-            "eyJhbGciOiJSUzI1NiIsImtpZCI6IjU5NjZjMjcyODJiZTJhYmQ1YThmNmU3NTFlYzU4MmUzIiwidHlwIjoiSldUIn0.eyJleHAiOjE2NDcyNzgwODcsImZhbWlseV9uYW1lIjoiQSIsImZ1bGxfbmFtZSI6IlBoYW5pIEEiLCJnaXZlbl9uYW1lIjoiUGhhbmkiLCJoZWFyaW5nX2lkIjpbXSwiaWF0IjoxNjQ3Mjc0NDg3LCJpc3MiOiJhdXRoU2VydmljZSIsIm5iZiI6MTY0NzI3NDQ4Nywic2Vzc2lvbl9pZCI6W10sInVzZXJfZW1haWwiOiJwaGFuaTI5NTg4QGdtYWlsLmNvbSIsInVzZXJfaWQiOiI3NGRlZGFhYS1iOTY1LTQyMWMtYTMxYy05Nzc5M2JhN2YzOTgifQ.Bz7pY7mc4CxjZA4qGO7O1qtDJ_aiohvxjZNkf3qwHCWIKGADIcGPqU9updy6YewhpVHNvy2kwlMecrx1fdJ8SvYh7Yyeki2vHD1fJD5jvKUhYj5Q-h-x6dV8FuHdeKuuaNhxhp9hg-7XoyctLNT-JZ0J1HrUws4WJD_qlDuIrnO-xPlcanAHMsZV9FA3wsVt3McUswg-QL-3f5Ygh5QrL0Qx2AnWAuedElPlcqvRSBlA56ErFVFiHriDj59wzXn2iX_OVKXlCaf-aSmRel646ZLOogggA0fx11yljdi_t6fERvn6z4Mu4puq2sW1_sJQgsZqa0tfy2eacs6xpAyqr_SIKjPoNcPM6YXYjPMsI3n7DRcGsOExWZkxrzih5Lw5NP14mpjJD_PgVBQ8CNyhD5f28lYj2m-O7kPXEy21PAXmIy3KmC0CEZYatzAIdWEZbE3BgP3mMgJA08fYr3ShtYKFbH1UZlsWiVElSLXVeXVAvW5JB07pbtjAYFhanXGL";
+            "eyJhbGciOiJSUzI1NiIsImtpZCI6IjU5NjZjMjcyODJiZTJhYmQ1YThmNmU3NTFlYzU4MmUzIiwidHlwIjoiSldUIn0.eyJleHAiOjE2NDcyODU3NjksImZhbWlseV9uYW1lIjoiQSIsImZ1bGxfbmFtZSI6IlBoYW5pIEEiLCJnaXZlbl9uYW1lIjoiUGhhbmkiLCJoZWFyaW5nX2lkIjpbXSwiaWF0IjoxNjQ3MjgyMTY5LCJpc3MiOiJhdXRoU2VydmljZSIsIm5iZiI6MTY0NzI4MjE2OSwic2Vzc2lvbl9pZCI6W10sInVzZXJfZW1haWwiOiJwaGFuaTI5NTg4QGdtYWlsLmNvbSIsInVzZXJfaWQiOiI3NGRlZGFhYS1iOTY1LTQyMWMtYTMxYy05Nzc5M2JhN2YzOTgifQ.x2S4vHddBpMg4gFyQ0XQsadUFH-OjmBuEQF8odGrj75n6a_JIx_YOca-vLTZsHLoG4xeHKqM_G2IKSlPS__HSQ38v0QPmyNhO_Z7LBvp7vU0ATK_BDEQhDVDxoDH-J1rjRGp8nWSjCEw0nxvimQLcacFt4yX1upOv33ul6rqi4oniJ8KiAajkUl6e-8f8RWvBhPOJDg1YqRYpXfjIa5Rod2k10HlmiQ_-KumLlPgZCVPnlouVYXoctyVIMpjNPZQH8CyxSOFTX2qMt9aPa3DbvKweXHIJZEt9MrcGperRNc_lW0OPng5ji-UPiOJannAZxJkUTKDVrAhPXSfe6uVZTys35vZmpjCzFlau31v95W1ZvspySktmolKWXOdwlBpCmIJD2nvOTICOPfjwN08R3qJ2JOA3g0Q1QQLUNY8bUAr3NImiLz5qLW7NpzjrN3t-Nc5GZiLm_GIzVDY33rJR0jq8wVGr8yDejgIXqVqfRGwqIHEblXSozXv97OFBzJP";
         let parsedClaim = TokenParser.ValidateToken(token);
 
         expect(parsedClaim).toStrictEqual(data);
