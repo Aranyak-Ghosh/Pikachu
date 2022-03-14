90e78e97c10b37ec9fcba3aeb6a20a083fa209d0
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-03-14 19:51:00 +0400

    Placeholder functions

--- services/SocketService.ts
+++ services/SocketService.ts
@@ -1,5 +1,17 @@
-'use strict';
+"use strict";
 
 class SocketService {
-    
+    constructor() {}
+
+    public async RegisterUserSocket() {
+        throw new Error("NotImplementedException");
+    }
+
+    public async GetSocketUser() {
+        throw new Error("NotImplementedException");
+    }
+
+    public async RemoveUserSocket() {
+        throw new Error("NotImplementedException");
+    }
 }
