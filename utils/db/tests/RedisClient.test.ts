684e943a1f06abee576d834fec1f1fc4fe2218c5
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-03-14 14:23:01 +0400

    Initial set of tests

--- utils/db/tests/RedisClient.test.ts
+++ utils/db/tests/RedisClient.test.ts
@@ -5,6 +5,33 @@ afterAll((done) => {
 });
 
 describe("RedisClientTests", () => {
+    it("should subscribe to channel and consume buffer", async () => {
+        var client = RedisClient.Initialize();
+        var channel = "foo";
+        var data = {
+            foo: "bar",
+        };
+
+        client.SubscribeToChannel(channel, function(message:string){
+            expect(message).toBe(
+                JSON.stringify(data)
+            );
+        })
+        client.Disconnect()
+    })
+
+    it("should publish to channel as buffer", async () => {
+        var client = RedisClient.Initialize();
+        var channel = "foo";
+        var data = {
+            foo: "bar",
+        };
+
+        await client.PublishMessage(channel, data);
+        client.Disconnect();
+        return;
+    });
+
     it("should write value as buffer to redis", async () => {
         var client = RedisClient.Initialize();
         const key = "foo";
@@ -26,18 +53,6 @@ describe("RedisClientTests", () => {
             JSON.stringify(value)
         );
 
-        client.Disconnect();
-        return;
-    });
-
-    it("should publish to channel as buffer", async () => {
-        var client = RedisClient.Initialize();
-        var channel = "foo";
-        var data = {
-            foo: "bar",
-        };
-
-        await client.PublishMessage(channel, data);
         client.Disconnect();
         return;
     });
