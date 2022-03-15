89bf9ef8d32db409319abfca6508f48f79104df6
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-03-15 13:44:07 +0400

    Added disconnect handler

---
+++ handlers/DiscconectedHandler.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+import SocketService from "services/SocketService";
+import { AGServerSocket } from "socketcluster-server";
+import { ILogger } from "utils/logger/interface/ILogger";
+
+async function onClientDisconnected(
+    socket: AGServerSocket,
+    disconnectCode: number,
+    disconnectReason: string,
+    logger: ILogger
+) {
+    logger.Info(
+        "Socket with socketId {0} disconnected with code: {1} reason: {2}",
+        socket.id,
+        disconnectCode,
+        disconnectReason
+    );
+
+    if (socket.authToken && socket.authToken!.Id) {
+        let service = SocketService.GetInstance();
+        let userSocket = await service.GetSocketUser(socket.id);
+        //TODO: Queue user disconnected message
+        await service.RemoveUserSocket(userSocket!.UserId);
+    }
+}
+
+export default onClientDisconnected;
