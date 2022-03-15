937a05834aabde1705db84fb57de6f74e4bc7550
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-03-15 10:58:00 +0400

    Added socket service

--- services/SocketService.ts
+++ services/SocketService.ts
@@ -5,36 +5,114 @@ import { AGServerSocket } from "socketcluster-server";
 import { RedisClient } from "../utils/db/RedisClient";
 import { SessionUser } from "../types/SocketConnection";
 import { RedisUserEntry } from "../types/RedisEntries";
+import { ILogger } from "utils/logger/interface/ILogger";
 class SocketService {
     private _instanceName: string;
-    constructor(instanceName: string) {
+    private _logger: ILogger;
+
+    private socketManager: SocketManager | null;
+    private redisClient: RedisClient | null;
+
+    constructor(instanceName: string, logger: ILogger) {
         this._instanceName = instanceName;
+        this._logger = logger;
+
+        this.socketManager = SocketManager.GetInstance() ?? null;
+        this.redisClient = RedisClient.GetInstance() ?? null;
     }
 
     public async RegisterUserSocket(
         socketInstance: AGServerSocket,
         sessionUser: SessionUser
     ) {
-        let socketManager = SocketManager.GetInstance();
-        if (socketManager != undefined && socketManager != null) {
-            socketManager.RegisterSocket(sessionUser.Id, socketInstance);
+        this._logger.Info(
+            "Registering user with id {0} to cache and socket manager",
+            sessionUser.Id
+        );
+        if (this.socketManager != undefined && this.socketManager != null) {
+            this.socketManager.RegisterSocket(socketInstance);
         }
 
-        await this.addSocketInstanceToCache(sessionUser);
+        await this.addSocketInstanceToCache(sessionUser, socketInstance.id);
     }
 
-    public async GetSocketUser() {
-        throw new Error("NotImplementedException");
+    public async GetSocketUser(userId: string): Promise<AGServerSocket | null> {
+        try {
+            let redisUser = await this.getSocketInstanceFromCache(userId);
+            let socket = this.socketManager?.GetSocketForUser(
+                redisUser.SocketId
+            );
+            return socket ?? null;
+        } catch (ex) {
+            this._logger.Error(
+                "Failed to fetch user socket for id {0}",
+                userId
+            );
+            throw ex;
+        }
     }
 
-    public async RemoveUserSocket() {
-        throw new Error("NotImplementedException");
+    public async RemoveUserSocket(userId: string) {
+        await this.removeSocketInstanceFromCache(userId);
     }
 
-    private async addSocketInstanceToCache(sessionUser: SessionUser) {
+    private async getSocketInstanceFromCache(
+        userId: string
+    ): Promise<RedisUserEntry> {
+        let client = this.redisClient;
+        try {
+            let serializedRedisEntry = await client!.GetValueForKey(userId);
+            let redisEntry: RedisUserEntry = JSON.parse(
+                serializedRedisEntry ?? ""
+            );
+            return redisEntry;
+        } catch (ex) {
+            this._logger.Error(
+                "Unable to read user info for userid {0} from cache",
+                userId,
+                ex
+            );
+            throw ex;
+        }
+    }
+
+    private async removeSocketInstanceFromCache(userId: string) {
+        let client = this.redisClient;
+        try {
+            let serializedRedisEntry = await client!.GetValueForKey(userId);
+            let redisEntry: RedisUserEntry = JSON.parse(
+                serializedRedisEntry ?? ""
+            );
+
+            let promises: Array<Promise<void>> = new Array<Promise<void>>();
+            promises.push(client!.ExpireKeyValue(userId));
+            redisEntry.Hearings.forEach((hearingId) => {
+                promises.push(client!.AddToSet(hearingId, userId));
+            });
+
+            redisEntry.Sessions.forEach((sessionId) => {
+                promises.push(client!.AddToSet(sessionId, userId));
+            });
+
+            await Promise.all(promises);
+        } catch (ex) {
+            this._logger.Error(
+                "Unable to read/remove user info for userid {0} from cache",
+                userId,
+                ex
+            );
+            throw ex;
+        }
+    }
+
+    private async addSocketInstanceToCache(
+        sessionUser: SessionUser,
+        SocketID: string
+    ) {
         let redisEntry: RedisUserEntry;
 
         redisEntry = {
+            SocketId: SocketID,
             Type: sessionUser.Type,
             State: sessionUser.State,
             Id: sessionUser.Id,
@@ -46,15 +124,17 @@ class SocketService {
             LastName: sessionUser.LastName,
         };
 
-        let redisClient = RedisClient.GetInstance();
-        if (redisClient != undefined && redisClient != null) {
+        if (this.redisClient != undefined && this.redisClient != null) {
             let promises: Array<Promise<void>> = new Array<Promise<void>>();
-
+            let redisClient = this.redisClient;
             promises.push(
-                redisClient.SetKeyValueBuffered(sessionUser.Id, redisEntry)
+                this.redisClient.SetKeyValueBuffered(sessionUser.Id, redisEntry)
             );
             promises.push(
-                redisClient.SetKeyValueBuffered(sessionUser.Email, redisEntry)
+                this.redisClient.SetKeyValueBuffered(
+                    sessionUser.Email,
+                    redisEntry
+                )
             );
 
             sessionUser.HearingIds.forEach((hearingId) => {
